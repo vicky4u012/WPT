fe7161217820886027b6f776cfc6281965027ea6
vicky4u012 <69521832+vicky4u012@users.noreply.github.com>
2020-09-09 22:23:58 +0530

    Delete pri-commit.sh

--- pri-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $GIT_AUTHOR_NAME == 'root' ]]
-then
-    echo "If you commit as root, you're gonna have a bad time";
-    echo "Set 'git config user.name' and try again";
-    exit 1;
-fi
